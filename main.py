786dad9f3208ee6b623d24b619669ee48dcf4a7d
Aslıhan <132354955+Aslihan16@users.noreply.github.com>
2023-09-19 20:45:28 +0300

    Add database

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import cv2
 import numpy as np
 import time
+import pypyodbc
+from django.utils.translation import gettext as _
+from django.http import HttpResponse
 
-net=cv2.dnn.readNet("yolov3.weights","yolov3.cfg")
+db = pypyodbc.connect(
+    'Driver={SQL Server};'
+    'Server=;DESKTOP-SBKTVS0'
+    'Database=pythonProject2.db;'
+    'Trusted_Connection=True;'
+)
+imlec = db.cursor()
+imlec.execute('SELECT * FROM  pythonProject2.dbo.tbl_image')
+imagetable = imlec.fetchall()
 
+for i in imagetable:
+    print(i)
 
-with open("coco.names.txt","r") as f:
-    classes=[line.strip()for line in f.readlines()]
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+
+with open("coco.names.txt", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
 
 layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
-cap = cv2.VideoCapture(0)
+cap= cv2.VideoCapture(0)
 
 font = cv2.FONT_HERSHEY_PLAIN
 starting_time = time.time()
@@ -19,54 +34,61 @@ frame_id = 0
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 while True:
-        _, frame = cap.read()
-        frame_id += 1
-height, width, channels = frame.shape
-blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
+    ret, frame = cap.read()
+    frame_id += 1
 
-# Showing information on the screen
+    height, width, channels = frame.shape
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs= net.forward(output_layers)
 
-class_ids = []
-confidences = []
-boxes = []
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.2:
-            # Object detected
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
 
-    # Rectangle coordinates
-x = int(center_x - w / 2)
-y = int(center_y - h / 2)
-boxes.append([x, y, w, h])
-confidences.append(float(confidence))
-class_ids.append(class_id)
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        confidence = confidences[i]
-        color = colors[class_ids[i]]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-        cv2.rectangle(frame, (x, y), (x + w, y + 30), color, -1)
-        cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 3, (255, 255, 255), 3)
 
-        elapsed_time = time.time() - starting_time
-        fps = frame_id / elapsed_time
-        cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), font, 3, (0, 0, 0), 3)
-        cv2.imshow("Image", frame)
-        key = cv2.waitKey(1)
 
-    if key == 27:
+    # Showing information on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.2:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
+
+    for i in range(len(boxes)):
+        if i in indexes:
+           x, y, w, h = boxes[i]
+           label = str(classes[class_ids[i]])
+           confidence = confidences[i]
+           color = colors[class_ids[i]]
+           cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+           cv2.rectangle(frame, (x, y), (x + w, y + 30), color, -1)
+           cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 3, (255, 255, 255), 3)
+
+    elapsed_time = time.time() - starting_time
+    fps = frame_id / elapsed_time
+    cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), font, 3, (0, 0, 0), 3)
+    cv2.imshow("Image", frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
